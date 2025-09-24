826ed58d42d43f43ed05d8aa572a381ba4e1c581
galvaodan <danilogalvao@protonmail.ch>
2025-09-23 22:00:01 -0300

    anoatual

---
+++ Programação de Computadores/2025.09.23/ano_atual = 2025.py
@@ -0,0 +1,20 @@
+ano_atual      = 2025
+
+ano_nascimento = 1991
+
+idade          = ano_atual - ano_nascimento
+
+print (idade)
+
+#print (idade)
+
+#print ('você tem', idade, 'anos.')
+
+#print ('você tem ' + str(idade) + ' anos')
+
+print(f'Você tem {idade} anos')
+
+
+print('Você nasceu em', ano_nascimento, 'e tem', idade, 'anos')
+print('Você nasceu em' + str(ano_nascimento) + 'e tem' + str(idade) + 'anos.')
+print(f'você nasceu em {ano_nascimento} e tem {idade}')
