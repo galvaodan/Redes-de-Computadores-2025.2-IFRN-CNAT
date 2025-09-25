5e5182ce58b23f7bc1f671791af3180c3c11b2aa
galvaodan <danilogalvao@protonmail.ch>
2025-09-24 21:52:09 -0300

    aula2025.09.24

---
+++ Programação de Computadores/2025.09.24/baseMaior = float(input('Informe o valor.py
@@ -0,0 +1,9 @@
+baseMaior = float(input('Informe o valor da Base Maior: '))
+
+baseMenor = float(input('Informe o valor da Base Menor: '))
+
+altura = float(input('Informe o valor da Altura: '))
+
+area = (baseMaior + baseMenor) * altura / 2
+
+print(f'A área do trapézio é {area:.1f}')
---
+++ Programação de Computadores/2025.09.24/exercico3.py
@@ -0,0 +1,4 @@
+n1 = input ('Informe o primeiro valor:')
+n2 = input ('Informe o segundo valor:')
+media = (n1 + n2)/ 2
+print(f'A Média do é igual a: {media:.1f}')
