fc9f4dbd7dc08931bda255c8170079b3693ceba4
galvaodan <danilogalvao@protonmail.ch>
2025-09-29 20:15:01 -0300

    Aula_29.09.2025

---
+++ Programação de Computadores/Média -IFRN.py
@@ -0,0 +1,10 @@
+intNota1 = int(input('Informe a nota da ETAPA 1:'))
+intNota2 = int(input('Informe a nota da ETAPA 2:'))
+
+intMedia = round((intNota1*2 + intNota2*3) / 5)
+
+print(f'Nota da Etapa 1: {intNota1}')
+print(f'Nota da Etapa 2: {intNota2}')
+print(f'Média: {intMedia}')
+if intMedia >= 60: print ('Aprovado')
+else: print ('Reprovado')
