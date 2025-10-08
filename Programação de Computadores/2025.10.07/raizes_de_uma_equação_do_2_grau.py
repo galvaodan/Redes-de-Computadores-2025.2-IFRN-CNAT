c048006f5ae27ab05f50fc74e2bc655e80b2e837
galvaodan <danilogalvao@protonmail.ch>
2025-10-07 21:54:19 -0300

    Atualizacao

---
+++ Programação de Computadores/2025.10.07/raizes_de_uma_equação_do_2_grau.py
@@ -0,0 +1,19 @@
+#entrada dos valores
+
+import sys
+valorA = float(input('Digite o valor de A: '))
+
+if valorA == 0:
+    sys.exit("Valor de A igual a 0. A equação não será do 2º grau.")
+
+ValorB = float(input('Digite o valor de B: '))
+ValorC = float(input('Digite o valor de C: '))
+
+# verificar se o valode de a é igual a zero, se for sair do programa.
+
+'''Verificar se o valor de a é zero. Caso seja, a equação não será do 
+2º grau e o programa deve informar o usuário sobre isso e encerrar;'''
+
+# Calcular o discriminante (delta) e, com base no valor de delta:
+
+floatDelta = 
