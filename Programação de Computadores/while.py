dd7de60d95c92c032717dc0015dcb92a12ac7095
galvaodan <danilogalvao@protonmail.ch>
2025-10-28 21:53:47 -0300

    20.10.2025

---
+++ Programação de Computadores/while.py
@@ -0,0 +1,7 @@
+intMultiplicando = 1
+intMultiplicador = 6
+
+while intMultiplicando <=10:
+    intProduto = intMultiplicando * intMultiplicador
+    print(f'{intMultiplicando} x {intMultiplicador} = {intProduto}')
+    intMultiplicando +=1
